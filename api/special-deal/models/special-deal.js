56c221e95e0148b3603393285294f4d12726609c
reasat-rafio <rafioar96@gmail.com>
2021-03-21 06:16:42 +0600

    commit?

--- api/special-deal/models/special-deal.js
+++ api/special-deal/models/special-deal.js
@@ -1,15 +1,8 @@
-const slugify = require("slugify");
-module.exports = {
-  lifecycles: {
-    beforeCreate: async (data) => {
-      if (data.name) {
-        data.slug = slugify(data.name);
-      }
-    },
-    beforeUpdate: async (params, data) => {
-      if (data.name) {
-        data.slug = slugify(data.name);
-      }
-    },
-  },
-};
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {};
